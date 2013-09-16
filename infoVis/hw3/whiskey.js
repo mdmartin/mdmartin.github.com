781d1f66c4cfc8f95de7e7d5d29d812f5f20587a
mdmartin <cn.md.martin@gmail.com>
2013-09-16 16:16:33 -0400

    a

---
+++ infoVis/hw3/whiskey.js
@@ -0,0 +1,31 @@
+var vis;
+var height = 300;
+var width = 600;
+
+d3.csv('./a3-CoffeeData.csv')
+
+function init(drinks) {
+	console.log(drinks[0].name)
+
+
+	xscale = d3.scale.linear()
+			   .domain([0, 
+			   		d3.max(drinks.map
+			   			(function(d) {return d.price}))])
+			   .range([0, width]);
+
+	yscale = d3.scale.linear()
+			   .domain()
+
+	vis = d3.select('#frame').append('svg')
+			.attr('width', width)
+			.attr('height', height);
+
+
+	vis.selectAll('circle')
+	   .
+
+
+
+
+}
